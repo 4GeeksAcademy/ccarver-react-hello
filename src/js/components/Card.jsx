ab85d0e2ebb8676a703efd61f7c1b09d790d6c1f
Seeker90 <chris.carver90@icloud.com>
2025-09-05 23:52:43 +0000

    practice

---
+++ src/js/components/Card.jsx
@@ -0,0 +1,20 @@
+import rigoImage from "../../img/rigo-baby.jpg"
+const cardSytle = {width:"18rem"}
+
+const Card = () => {
+    return (
+        <>
+            <div className="card" style={cardSytle}>
+                <img src={rigoImage} class="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+            
+        </>
+    );
+};
+
+export default Card;
